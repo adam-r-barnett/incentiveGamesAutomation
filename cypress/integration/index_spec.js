05f6df088812e0b77d74551a17b20fb5a0347726
adam-r-barnett <adam.r.barnett@hotmail.com>
2021-04-27 10:59:11 +0100

    refactoring tests to use custom cypress commands

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -3,9 +3,9 @@ const data = require("./data/data.json");
 describe('visit contact us homepage from index', () => {
     it('navigate to contact us page', () => {
       // Read URL from JSON 
-      cy.visit(data.indexPage)
-      // Find parent class and then find contact us element within that and click
-      cy.get('.nav-primary').contains('Contact Us').should('be.visible').click()
+      cy.visit(data.indexPageURL)
+      // Find parent class and then find "contact us" element within that and click
+      cy.visitContactUsURL()
     })
   })
   
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -22,4 +22,4 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     })
 
 // Alternatively you can use CommonJS syntax:
-// require('./commands')
+require('./commands')
